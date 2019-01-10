c585873e3819def4040fb8ca4839efbf4d4302d6
srchach <gsikmo@gmail.com>
2019-01-10 05:32:26 -0600

    moved modal code to a temporal component

---
+++ src/components/modalTemp.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="jumbotron">
+		<b-row>
+			<b-col cols="12" offset="0">
+				{{ msg }}
+				<br>
+			</b-col>
+		</b-row>
+		<b-row>
+			<b-col>
+				<b-btn v-b-modal.modal5>Qué se cuece aqui?</b-btn>
+
+				<!-- Modal Component -->
+				<b-modal id="modal5" title="Alerta!">
+					<p class="my-4">Sitio en construcción. Hombres trabajando xd</p>
+				</b-modal>
+			</b-col>
+		</b-row>
+	</div>
+</template>
+
+<script>
+	import bModal from 'bootstrap-vue/es/components/modal/modal'
+	export default {
+		data () {
+			return {
+				msg: 'Zoi un Jumbotron, y te doy la bienvenido a Maquila de Folio c:'
+			}
+		}
+	}
+</script>
+
+<style lang="scss"></style>
