0d74764b5004029782062d345dc4eff22e16903f
srchach <gsikmo@gmail.com>
2019-01-20 01:18:55 -0600

    Vue: Customized directives

--- src/main.js
+++ src/main.js
@@ -9,6 +9,14 @@ import App from './App.vue'
 Vue.use(BootstrapVue)
 Vue.use(Layout);
 
+
+//	Directivas de prueba. Usar esta como "v-decorar"
+Vue.directive('decorar', {
+	bind(elemento, modificadores, nodo_dom_virtual){
+		elemento.style.fontFamily = "times new roman"
+	}
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
