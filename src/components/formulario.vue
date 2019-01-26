1da8c6dbcb66d8a13e713952b3bc926238a5c606
srchach <gsikmo@gmail.com>
2019-01-26 01:42:08 -0600

    Select-options dinámico

--- src/components/formulario.vue
+++ src/components/formulario.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="jumbotron">
 		<b-form>
 			<b-form-group id="exampleInputGroup1"
 										label="Email address:"
@@ -20,25 +20,38 @@
 											placeholder="Mi pleisjolder"
 											v-model.trim="usuario.pleisjolder">
 				</b-form-input>
+				<br>
+				<b-form-select v-model="usuario.selected" :options="usuario.options" class="mb-3" />
+
 			</b-form-group>
 			<b-button type="submit" variant="primary">Submit</b-button>
 			<b-button type="reset" variant="danger">Reset</b-button>
 		</b-form>
 		<div>
-			{{ usuario.pleisjolder }}
+			Valor de campo con Trim: {{ usuario.pleisjolder }} <br>
+	    <div>Selected: <strong>{{ usuario.selected }}</strong></div>
 		</div>
 	</div>
 </template>
 
 <script>
 	import { Form } from 'bootstrap-vue/es/components';
+	import { FormSelect } from 'bootstrap-vue/es/components';
 
 	export default {
 		data(){
 			return {
 				usuario: {
 					pleisjolder: "",
-					email: ""
+					email: "",
+					selected: null,
+		      options: [
+		        { value: null, text: 'Please select an option' },
+		        { value: 'Mexico', text: 'País donde hay tacos c:' },
+		        { value: 'Venezuela', text: 'Pistaa: Van a salir de su crisis' },
+		        { value: 'Cuba', text: 'Uffff men 7u7r cerca y barato' },
+		        { value: 'Korea 7u7r', text: 'Paises asiaticos shidos :3' }
+		      ]
 				}
 			}
 		}
