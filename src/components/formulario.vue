5c6ac6348f22fbb06293d19b3efdf7ee04b8688e
srchach <gsikmo@gmail.com>
2019-01-26 11:50:36 -0600

    Added textarea. Experimenting with B-Vue components

--- src/components/formulario.vue
+++ src/components/formulario.vue
@@ -8,8 +8,7 @@
 				<b-form-input id="exampleInput1"
 											type="email"
 											required
-											placeholder="Enter email">
-				</b-form-input>
+											placeholder="Enter email"/>
 			</b-form-group>
 			<b-form-group id="exampleInputGroup2"
 										label="Your Name:"
@@ -18,18 +17,30 @@
 											type="text"
 											required
 											placeholder="Mi pleisjolder"
-											v-model.trim="usuario.pleisjolder">
-				</b-form-input>
-				<br>
-				<b-form-select v-model="usuario.selected" :options="usuario.options" class="mb-3" />
-
+											v-model.trim="usuario.pleisjolder"/>
+			</b-form-group>
+			<b-form-group	label="Elige tu país!" 
+										label-for="s_options">
+				<b-form-select	id="s_options" 
+												v-model="usuario.selected" 
+												:options="usuario.options" 
+												class="mb-3" />
+			</b-form-group>
+			<b-form-group>
+				<b-form-textarea id="textarea1"
+										 v-model="usuario.text"
+										 placeholder="Pon lo que sea!"
+										 :rows="3"
+										 :max-rows="6"/>
 			</b-form-group>
 			<b-button type="submit" variant="primary">Submit</b-button>
 			<b-button type="reset" variant="danger">Reset</b-button>
 		</b-form>
 		<div>
+			<br>
 			Valor de campo con Trim: {{ usuario.pleisjolder }} <br>
-	    <div>Selected: <strong>{{ usuario.selected }}</strong></div>
+			<div>Selected: <strong>{{ usuario.selected }}</strong></div>
+			Contenido del Textarea: <pre class="mt-3">{{ usuario.text }}</pre>
 		</div>
 	</div>
 </template>
@@ -45,13 +56,14 @@
 					pleisjolder: "",
 					email: "",
 					selected: null,
-		      options: [
-		        { value: null, text: 'Please select an option' },
-		        { value: 'Mexico', text: 'País donde hay tacos c:' },
-		        { value: 'Venezuela', text: 'Pistaa: Van a salir de su crisis' },
-		        { value: 'Cuba', text: 'Uffff men 7u7r cerca y barato' },
-		        { value: 'Korea 7u7r', text: 'Paises asiaticos shidos :3' }
-		      ]
+					text: '',
+					options: [
+						{ value: null, text: 'Please select an option' },
+						{ value: 'Mexico', text: 'País donde hay tacos c:' },
+						{ value: 'Venezuela', text: 'Pistaa: Van a salir de su crisis' },
+						{ value: 'Cuba', text: 'Uffff men 7u7r cerca y barato' },
+						{ value: 'Korea 7u7r', text: 'Paises asiaticos shidos :3' }
+					]
 				}
 			}
 		}
